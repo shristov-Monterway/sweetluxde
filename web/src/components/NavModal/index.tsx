6af0ef056c5b210bfd0fdc918d1ceedb90bc71de
Simeon Hristov <s.hristov@monterway.com>
2025-03-25 15:12:01 +0200

    Fix pipeline

--- web/src/components/NavModal/index.tsx
+++ web/src/components/NavModal/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { AbstractComponentType } from '../../types/AbstractComponentType';
 import Modal from '../Modal';
-import useApp from '../../hooks/useApp';
 
 export interface NavModalProps extends AbstractComponentType {
   showModal: boolean;
 }
 
 const NavModal = (props: NavModalProps): React.JSX.Element => {
-  const app = useApp();
-
   return (
     <Modal
       showModal={props.showModal}
