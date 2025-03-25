82a215136e1dfa94a6504dbfa0ee38d7c24fb59a
Simeon Hristov <s.hristov@monterway.com>
2025-03-25 18:06:51 +0200

    Update Config.ts

--- firebase/functions/src/Config.ts
+++ firebase/functions/src/Config.ts
@@ -19,7 +19,7 @@ const Config: ConfigType = {
     'sk_test_51NIyYzLadHO4qOyFcedEE9H3nKPkgQMi2KmW74Uj3VO6BxX7jLwKYpP2rRKztslkfHptWNXhJywz03eN2fR3iILb00KvJ9Z2Au',
   stripeWebhookSecretKey:
     'whsec_b7176a49daa031aa728f43b60c8af76f85022c7ef313f1e0492b0f8e17c3372f',
-  authenticationMethods: ['google', 'email', 'phone'],
+  authenticationMethods: ['google', 'email'],
   attributesToFilter: ['size'],
   headerHeight: 90,
   hasInvitations: true,
